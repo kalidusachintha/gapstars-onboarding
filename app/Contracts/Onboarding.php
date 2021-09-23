48e7301de9d836321e77bdfc20fbe0a5f2bd83b4
Your Name <you@example.com>
2021-09-23 07:16:05 +0530

    code refactering

--- app/Contracts/Onboarding.php
+++ app/Contracts/Onboarding.php
@@ -3,7 +3,14 @@ namespace App\Contracts;
 
 use Illuminate\Http\JsonResponse;
 
+/**
+ * should be implemented in to any service class that uses different sources for chart data
+ */
 interface Onboarding
 {
+    /**
+     * Generate precentage amount for each step that needs to chart
+     * @return JsonResponse
+     */
     public function getOnboardStepsPrecentage(): JsonResponse;
 }
