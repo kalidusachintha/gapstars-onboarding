cc0f6a7d04a592889a040cc2a70aacdc50e39abe
Your Name <you@example.com>
2021-09-27 08:00:27 +0530

    add test cases

--- tests/Feature/DataSourceTest.php
+++ tests/Feature/DataSourceTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DataSourceTest extends TestCase
@@ -12,7 +10,7 @@ class DataSourceTest extends TestCase
     /**
      * @return void
      */
-    public function test_csv_available_and_retrun_data()
+    public function test_csv_available_and_return_data()
     {
         $this->withoutExceptionHandling();
         $this->assertTrue(file_exists('storage/export.csv'));
@@ -20,6 +18,36 @@ class DataSourceTest extends TestCase
         $response = $this->json('GET', '/api/getall', [
                               'name' => 'test-name',
                               'data' => [100,90,89]
-                         ])->assertStatus(200);
+                         ]);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @return void
+     */
+    public function test_csv_available()
+    {
+        $this->withoutExceptionHandling();
+        $this->assertFalse(file_exists(''));
+    }
+
+    /**
+     * @return void
+     */
+    public function test_csv_return_raw_data()
+    {
+        $this->withoutExceptionHandling();
+        $this->assertTrue(file_exists('storage/export.csv'));
+        $data = [
+            'user_id'=> '0000',
+            'date' => '2020-10-10',
+            'onboard_precentage' => '40',
+            'start_date_of_week' => '2020-10-01'
+        ];
+
+        $this->assertNotEmpty(
+            $data,
+            "csv is empty"
+        );
     }
 }
